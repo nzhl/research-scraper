a5b1e1c976af2c693a8b678f8999bb1778936803
nzhl <77595103@qq.com>
2017-07-02 19:01:29 +0000

    finish the base version

--- web/florida/florida.py
+++ web/florida/florida.py
@@ -2,7 +2,6 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, current_app
 import pymysql 
 
-
 app = Flask(__name__)
 
 app.config.update(dict(
@@ -15,25 +14,43 @@ app.config.update(dict(
 ))
 
 
-@app.cli.command('initdb')
-def init_db():
-    '''Connect to mysql.'''
+@app.teardown_appcontext
+def close_db(error):
+    '''Close mysql connection'''
+    if hasattr(g, 'db'):
+        g.db.close()
+        print("close db")
+
+@app.route("/", methods=['get'])
+def index_page():
+    articles = []
+    query = request.args.get('query')
+    if query:
+        search_fields = ["authors", "title"]
+        for field in search_fields:
+            articles += select_field(field, query)
+
+    return render_template("index.html", articles=articles) 
+
+def get_db():
+    '''Get connection to mysql.'''
+
     db = pymysql.connect(host=current_app.config['HOST'],
                          user=current_app.config['USERNAME'],
                          password=current_app.config['PASSWORD'],
                          db=current_app.config['DB'],
-                         charset=current_app.config['CHARSET']
+                         charset=current_app.config['CHARSET'],
+                         cursorclass=pymysql.cursors.DictCursor
     )
-    g['db'] = db
-    print('Initialized the database.')
+    g.db = db
+    return db
 
-
-
-
-
-
-
-@app.route("/")
-def index_page():
-    return render_template("index.html") 
-
+def select_field(field, query):
+    sql = 'SELECT * FROM articles WHERE %s LIKE "%%%s%%";' % (field, query)
+    result = []
+    if not hasattr(g, 'db'):
+        get_db()
+    with g.db.cursor() as cursor:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    return result
